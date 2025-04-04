be85f991b72dd23f7e56f25de81e3bd537ce1825
t-ageru24 <tadesseageru13@gmail.com>
2025-04-04 19:44:59 +0300

    modified

--- frontend/src/pages/deleteBook.jsx
+++ frontend/src/pages/deleteBook.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-function deleteBook() {
+
+const DeleteBook = () => {
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const handleDeleteBook = () => {
+        setLoading(true);
+        axios
+            .delete(`http://localhost:5555/books/${id}`)
+            .then((response) => {
+                setLoading(false);
+                navigate('/');
+            });
+    }
     return (
         <div>deleteBook</div>
     )
 }
 
-export default deleteBook
+export default DeleteBook
