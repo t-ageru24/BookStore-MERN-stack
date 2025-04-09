84c2143193727b519dd0d76ab90eb22d49ec3392
Tadesse Ageru <81475801+t-ageru24@users.noreply.github.com>
2025-04-09 23:23:46 +0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
     useEffect(() => {
         setLoading(true);
         axios
-            .get('http://localhost:5555/books')
+            .get('https://book-store-app-backend-vdzd.onrender.com/books')
             .then((response) => {
                 setBooks(response.data.data);
                 setLoading(false);
