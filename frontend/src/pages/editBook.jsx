857c665ba8fd31d38af0a8b177c397bdc7c3e708
Tadesse Ageru <81475801+t-ageru24@users.noreply.github.com>
2025-04-09 23:22:16 +0300

    Update editBook.jsx

--- frontend/src/pages/editBook.jsx
+++ frontend/src/pages/editBook.jsx
@@ -17,7 +17,7 @@ const EditBook = () => {
 
     useEffect(() => {
         setLoading(true);
-        axios.get(`http://localhost:5555/books/${id}`)
+        axios.get(`https://book-store-app-backend-vdzd.onrender.com/books/${id}`)
             .then((response) => {
                 setAuthor(response.data.author);
                 setPublishYear(response.data.publishYear);
